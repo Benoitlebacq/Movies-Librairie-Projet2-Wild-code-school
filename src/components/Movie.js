78a6144cd59d8b2a4000c988a529be9cbce858ad
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-04-05 14:38:59 +0200

    adding testpage

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ class Movie extends React.Component {
   render() {
     return (
       <div className= "col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3 mt-1 ">
+      <span>{this.props.title}</span>
         <Card >
           <Link  to={"/fiche"}><CardImg src ={this.props.image}/></Link>
         </Card>
