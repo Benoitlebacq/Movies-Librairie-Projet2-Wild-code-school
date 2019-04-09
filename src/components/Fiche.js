eed04388c1ecfcca2b0ec6f14eedc5e68b2995aa
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-04-09 17:57:46 +0200

    ajout de details dans fiche.js

--- src/components/Fiche.js
+++ src/components/Fiche.js
@@ -18,6 +18,7 @@ class Fiche extends React.Component {
   }
 
   //on veut recuperer dans la fiche :  "https://api.themoviedb.org/3/movie/ + id de la fiche qui a été cliquée + ?api_key=a8a3380a564299f359c18e52aaa5bc79"
+  // et pourles credits "https://api.themoviedb.org/3/movie/ + id de la fiche qui a été cliquée + / credits + ?api_key=a8a3380a564299f359c18e52aaa5bc79"
   // if video === false  on fait une recherche du trailer  sur YT
 
   export default Fiche;
