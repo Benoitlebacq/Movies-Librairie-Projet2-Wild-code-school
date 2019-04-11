2532118fac07428f6a58a2a4f7a089ac7d04fc96
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-04-11 17:03:57 +0200

    Just changing an extra letter in index.html

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -31,7 +31,7 @@ const Data =
   url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_genres=16"     
 },
 {type: "Documentary",          
-  url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_genres=99"     
+  url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_genres=99&page=2"     
 },
 {type: "War",          
   url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_genres=10752"     
@@ -45,18 +45,10 @@ url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18
 {type: "History",          
   url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_genres=36"     
 },
-{type: "Best of 1999 18+",          
-  url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&primary_release_year=1999&sort_by=vote_average.desc&include_adult=true"     
-},
 {type: "Best of French language",          
   url : "https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&with_original_language=fr"     
 }
 
 ]
   export default Data;
-  //"https://api.themoviedb.org/3/genre/movie/list?api_key=a8a3380a564299f359c18e52aaa5bc79" 
-  // faire un menu déroulant des genre avec un li + map dans un composant
-
-  // on peut aller chrcher un tableau qui referencie les trailer videos des films sur YT : 
-  // https://api.themoviedb.org/3/movie/287947/videos?api_key=a8a3380a564299f359c18e52aaa5bc79
-  // exemple de filtre : https://api.themoviedb.org/3/discover/movie?api_key=a8a3380a564299f359c18e52aaa5bc79&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
+ 
