0605820c722411a50ae84d152ad3ca4db126d638
Antoine2357 <antoine.jacqmin@live.fr>
2019-04-15 10:58:20 +0200

    add transition effect on routing

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import HomePage from "./HomePage"
 import Fiche from "./Fiche"
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 function Routing({ location }) {
     return (
       <div>
+        <TransitionGroup className="transition-group">
+          <CSSTransition
+            key={location.key}
+            timeout={{ enter: 300, exit: 300 }}
+            classNames="fade"
+          >
             <section className="route-section">
               <Switch location={location}>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/fiche/:ficheNumber(\d+)" component={Fiche} />
               </Switch>
             </section>
+            </CSSTransition>
+        </TransitionGroup>
       </div>
     );
   }
