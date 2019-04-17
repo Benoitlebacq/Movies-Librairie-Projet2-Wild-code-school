c5af48123e0ec6c1d91b9e5d4fd08d26f0d6ce9b
Louis Allard <l.allard@yahoo.fr>
2019-04-17 10:43:02 +0200

    refresh navbar to navlink method and minor correction on app.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Dropdown, DropdownItem, DropdownToggle, DropdownMenu, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import '../App.css';
 class Header extends React.Component {
   constructor(props) {
@@ -16,46 +17,43 @@ class Header extends React.Component {
     });
   }
   render() {
-    return ( 
-      <Fragment>       
+    return (
+      <Fragment>
         <div className="menu">
           <nav className="navbar navbar-expand-xl navbar-light">
-            
-            <img className="logo"  src="https://image.noelshack.com/fichiers/2019/15/5/1555076151-screenshot-from-2019-04-12-11-21-07-convertimage-1.png" />
-            
-           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-          </button> 
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav mx-auto">
-                <li className="nav-item"><a className="nav-link" href="#Science Fiction">Science-Fictions</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Westerns">Westerns</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Drama">Drama</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Comedy">Comedy</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Thriller">Thriller</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Horror">Horror</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Animation">Animation</a></li>
-                <li className="nav-item"><a className="nav-link" href="#Documentary">Documentary</a></li>
-                <li className="nav-item"><a className="nav-link" href="#War">War</a></li>
-                <li className="nav-item"><a className="nav-link" href="#History">History</a></li>
-                <li className="nav-item">
-                  <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                    <DropdownToggle nav caret>
-                      Listes
+            <ul>
+              <li><NavLink activeClassName="active" exact to="/">
+                <img className="logo" src="https://image.noelshack.com/fichiers/2019/15/5/1555076151-screenshot-from-2019-04-12-11-21-07-convertimage-1.png" />
+                </NavLink></li>
+           </ul>
+            <ul className="navbar-nav mx-auto">
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=878">Science Fiction</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=37">Westerns</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=18">Drama</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=35">Comedy</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=53">Thriller</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=27">Horror</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=16">Animation</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=99">Documentary</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=10752">War</NavLink></li>
+              <li><NavLink activeClassName="active" className="nav-link" exact to="/genres=36">History</NavLink></li>
+              <li className="nav-item">
+                <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                  <DropdownToggle nav caret>
+                    Listes
                 </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem>Mes favoris</DropdownItem>
-                      <DropdownItem >A regarder plus tard</DropdownItem>
-                      <DropdownItem>Autres liste</DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>+ Nouvelle liste</DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                 </li> 
-              </ul>
-            </div>
+                  <DropdownMenu>
+                    <DropdownItem>Mes favoris</DropdownItem>
+                    <DropdownItem >A regarder plus tard</DropdownItem>
+                    <DropdownItem>Autres liste</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>+ Nouvelle liste</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </li>
+            </ul>
           </nav>
-          </div>
+        </div>
         <div className="recherche">
           <InputGroup>
             <InputGroupAddon addonType="prepend">@</InputGroupAddon>
